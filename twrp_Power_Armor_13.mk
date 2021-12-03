680bd65ffb3cefe9422688e1fb51efd8230da1c7
lopestorm <lopestom1@gmail.com>
2021-12-02 22:19:10 -0300

    Update twrp_Power_Armor_13.mk

--- twrp_Power_Armor_13.mk
+++ twrp_Power_Armor_13.mk
@@ -52,6 +52,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
